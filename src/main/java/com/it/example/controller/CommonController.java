c9bbbd6da0289524e5375703e266d7742e4bc60d
wjh <1094613417@qq.com>
2023-08-14 10:20:47 +0800

    文件下载。文件IO

--- src/main/java/com/it/example/controller/CommonController.java
+++ src/main/java/com/it/example/controller/CommonController.java
@@ -3,12 +3,17 @@ package com.it.example.controller;
 import com.it.example.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -59,5 +64,38 @@ public class CommonController {
         return R.success(fileName);
     }
 
+    /**
+     * 文件下载
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+        try {
+            //输入流 通过输入流获取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath+name));
+
+            //输出流，通过输出流将文件写回浏览器，在浏览器展示图片
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            //设置文件格式为图片
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes))!=-1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
 
 }
